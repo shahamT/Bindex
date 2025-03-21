b751f6e906882e8e9dd3efdc72c4aaa74857ff96
Shaham Tamir <shahamt@gmail.com>
2025-03-21 15:38:09 +0200

    remove dummy table from html

--- js/services/book.model.js
+++ js/services/book.model.js
@@ -9,12 +9,12 @@ const gBooks = [
         imgUrl: ''
     },
     {
-        id: 'bg4J78',
-        title: 'The adventures of Lori Ipsi',
-        author: 'blah blah',
-        price: 120,
-        rate: 4,
-        stock: 5,
+        id: 'bg4J79',
+        title: 'Harry Potter',
+        author: 'j.k rolling',
+        price: 10,
+        rate: 5,
+        stock: 12,
         imgUrl: ''
     }
 ]

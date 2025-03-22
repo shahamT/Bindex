45306f0f5781e1f3b6273df370c92232ff32ed7b
Shaham Tamir <shahamt@gmail.com>
2025-03-22 22:44:02 +0200

    Add pop-up msg for success and fail and empty state for books table

--- js/book.controller.js
+++ js/book.controller.js
@@ -21,26 +21,33 @@ function renderBooksTable(books) {
                     </div>
 
                     <div class="table-items-container">`
-    var booksStrHTMLs = books.map(book => {
-        var bookHTML = `
-                <div class="table-row">
+    if (books.length > 0) {
+        var booksStrHTMLs = books.map(book => {
+            var bookHTML = `
+                <div class="table-row" onclick="onViewBookClick('${book.id}')">
                     <div class="table-cell cell1">${book.title}</div>
                     <div class="table-cell cell2">${book.author}</div>
                     <div class="table-cell cell3">${book.price}$</div>
                     <div class="table-cell cell4">${(book.rate).toFixed(1)}⭐</div>
                     <div class="table-cell cell5">${book.stock}</div>
                     <div class="table-cell cell6 item-btns">
-                        <button class="txt-btn btn-secondary" onclick="onViewBtnClick('${book.id}')">View</button>
-                        <button class="txt-btn btn-primary" onclick="onUpdateBtnClick('${book.id}')">Edit</button>
-                        <button class="icon-btn btn-secondary" onclick="onDeleteBook('${book.id}')"><svg viewBox="0 0 482.428 482.429" xmlns="http://www.w3.org/2000/svg"><g><g><path d="M381.163,57.799h-75.094C302.323,25.316,274.686,0,241.214,0c-33.471,0-61.104,25.315-64.85,57.799h-75.098c-30.39,0-55.111,24.728-55.111,55.117v2.828c0,23.223,14.46,43.1,34.83,51.199v260.369c0,30.39,24.724,55.117,55.112,55.117h210.236c30.389,0,55.111-24.729,55.111-55.117V166.944c20.369-8.1,34.83-27.977,34.83-51.199v-2.828C436.274,82.527,411.551,57.799,381.163,57.799z M241.214,26.139c19.037,0,34.927,13.645,38.443,31.66h-76.879C206.293,39.783,222.184,26.139,241.214,26.139z M375.305,427.312c0,15.978-13,28.979-28.973,28.979H136.096c-15.973,0-28.973-13.002-28.973-28.979V170.861h268.182V427.312z M410.135,115.744c0,15.978-13,28.979-28.973,28.979H101.266c-15.973,0-28.973-13.001-28.973-28.979v-2.828c0-15.978,13-28.979,28.973-28.979h279.896c15.973,0,28.973,13.001,28.973,28.979V115.744z"/><path d="M191.479,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083c-7.225,0-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083C185.621,401.819,191.479,395.961,191.479,388.736z"/><path d="M264.755,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083s-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083S264.755,395.961,264.755,388.736z"/><path d="M338.031,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083c-7.226,0-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083C332.174,401.819,338.031,395.961,338.031,388.736z"/></g></g></svg>
+                        <button class="txt-btn btn-primary" onclick="onUpdateBtnClick('${book.id}',event)">Edit</button>
+                        <button class="icon-btn btn-secondary" onclick="onDeleteBook('${book.id}',event)"><svg viewBox="0 0 482.428 482.429" xmlns="http://www.w3.org/2000/svg"><g><g><path d="M381.163,57.799h-75.094C302.323,25.316,274.686,0,241.214,0c-33.471,0-61.104,25.315-64.85,57.799h-75.098c-30.39,0-55.111,24.728-55.111,55.117v2.828c0,23.223,14.46,43.1,34.83,51.199v260.369c0,30.39,24.724,55.117,55.112,55.117h210.236c30.389,0,55.111-24.729,55.111-55.117V166.944c20.369-8.1,34.83-27.977,34.83-51.199v-2.828C436.274,82.527,411.551,57.799,381.163,57.799z M241.214,26.139c19.037,0,34.927,13.645,38.443,31.66h-76.879C206.293,39.783,222.184,26.139,241.214,26.139z M375.305,427.312c0,15.978-13,28.979-28.973,28.979H136.096c-15.973,0-28.973-13.002-28.973-28.979V170.861h268.182V427.312z M410.135,115.744c0,15.978-13,28.979-28.973,28.979H101.266c-15.973,0-28.973-13.001-28.973-28.979v-2.828c0-15.978,13-28.979,28.973-28.979h279.896c15.973,0,28.973,13.001,28.973,28.979V115.744z"/><path d="M191.479,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083c-7.225,0-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083C185.621,401.819,191.479,395.961,191.479,388.736z"/><path d="M264.755,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083s-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083S264.755,395.961,264.755,388.736z"/><path d="M338.031,388.736V214.433c0-7.225-5.857-13.083-13.083-13.083c-7.226,0-13.083,5.858-13.083,13.083v174.303c0,7.225,5.857,13.083,13.083,13.083C332.174,401.819,338.031,395.961,338.031,388.736z"/></g></g></svg>
                         </button>
                     </div>
                 </div>`
 
-        return bookHTML
-    })
+            return bookHTML
+        })
+        strHTML += booksStrHTMLs.join('')
+    }
+    else {
+        strHTML += `<div class="empty-state-container">
+                        <img class="empty-state-img" src="img/no-books-empty-state.png" alt="no books">
+                        <p>No books were found...</p>
+                        </div>`
+    }
 
-    strHTML += booksStrHTMLs.join('')
     strHTML += `</div>`
 
     elTableGrid.innerHTML = strHTML
@@ -55,7 +62,8 @@ function onAddBookBtnClick() {
     OpenSidePanel('add')
 }
 
-function onUpdateBtnClick(bookId) {
+function onUpdateBtnClick(bookId,ev) {
+    if(ev) ev.stopPropagation()
     const book = getBookById(bookId)
     OpenSidePanel()
     renderUpdateBook(book)
@@ -183,6 +191,7 @@ function onAddBook() {
     addBook(book)
     onCloseSidePanel()
     renderBooks()
+    showPopUpMsg(`success`, `You have added a new book!`)
 }
 
 function onUpdateBook(bookId) {
@@ -199,13 +208,15 @@ function onUpdateBook(bookId) {
     updateBook(bookId, data)
     onCloseSidePanel()
     renderBooks()
+    showPopUpMsg(`success`, `You have succesfully updated this book!`)
 }
 
-function onDeleteBook(bookId) {
+function onDeleteBook(bookId,ev) {
+    if(ev) ev.stopPropagation()
     renderConfirmDelete(bookId)
 }
 
-function onViewBtnClick(bookId) {
+function onViewBookClick(bookId) {
     const book = getBookById(bookId)
     renderViewBook(book)
     OpenSidePanel()
@@ -273,6 +284,7 @@ function onDeleteConfirm(bookId) {
         elBackdrop.innerHTML = ``
     }, 500)
 
+    showPopUpMsg(`success`, `You have succesfully deleted this book!`)
 }
 
 function onCloseModal() {
@@ -283,4 +295,24 @@ function onCloseModal() {
         elBackdrop.hidden = true
         elBackdrop.innerHTML = ``
     }, 500)
+}
+
+function showPopUpMsg(type = `success`, msg = `Success!`) {
+    const elPopUp = document.querySelector(`.pop-up-msg`)
+    elPopUp.classList.add(type)
+    const elMsg = document.querySelector(`.pop-up-msg p`)
+    elMsg.innerText = msg
+
+    elPopUp.hidden = false
+    elPopUp.offsetHeight
+    elPopUp.classList.add(`apperance`)
+
+    setTimeout(() => {
+        elPopUp.classList.remove(`apperance`)
+        setTimeout(() => {
+            elPopUp.hidden = false
+            elMsg.innerText = ``
+            elPopUp.classList.remove(`fail`, `success`)
+        }, 500);
+    }, 2500);
 }
